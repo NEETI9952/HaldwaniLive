208c21af449e83fd956363bbc8dfcb1634959890
Neeti9952 <neetibisht919@gmail.com>
2024-02-18 23:18:35 +0530

    adding permissions in manifest

--- lib/Pages/article_view.dart
+++ lib/Pages/article_view.dart
@@ -2,37 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
-  String blogUrl;
-  ArticleView({required this.blogUrl});
+  final String blogUrl;
+
+  ArticleView({required
+  this.blogUrl});
 
   @override
   State<ArticleView> createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+  bool _isLoading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("हल्द्वानी",
-                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),//खबर
-              Text(
-                "लाइव    ",
-                style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          centerTitle: true,
-          elevation: 0.0,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("हल्द्वानी",
+              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),//खबर
+            Text(
+              "लाइव    ",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            )
+          ],
         ),
-        body: Container(
-          child: WebView(
-            initialUrl:widget.blogUrl ,
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: widget.blogUrl,
             javascriptMode: JavascriptMode.unrestricted,
+            onPageFinished: (_) {
+              setState(() {
+                _isLoading = false;
+              });
+            },
+            onPageStarted: (_) {
+              setState(() {
+                _isLoading = true;
+              });
+            },
+            onWebResourceError: (error) {
+              // Handle web resource errors
+            },
           ),
-        ));
+          if (_isLoading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
+      ),
+    );
   }
 }
